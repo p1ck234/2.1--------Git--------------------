b826ec42631ac1205a61887358ee768d65afc637
p1ck234 <danil-vetrov47@email.com>
2023-11-09 21:25:12 +0400

    made five assignments

--- srcript.js
+++ srcript.js
@@ -1,4 +1,26 @@
+// 1
 let a = 10;
 alert(a);
 a = 20;
 alert(a);
+
+//2
+let ageIphone = 2017;
+alert(ageIphone);
+
+//3
+let nameJs = "Brendan Eich";
+alert(nameJs);
+
+//4
+let b = 10;
+let c = 20;
+alert(
+  `Сумма = ${b + c}\nРазность = ${b - c}\nПроизведение = ${b * c}\nЧастное = ${
+    b - c
+  }`
+);
+
+//5
+let result = 2 ** 5;
+alert(result);
