9befe73ce9860ab0fccdaa41398d48ef3f586b51
p1ck234 <danil-vetrov47@email.com>
2023-11-20 19:46:32 +0400

    made two assignments

---
+++ hw-4.js
@@ -0,0 +1,20 @@
+//1
+const less = (a, b) => {
+  if (a < b) {
+    return a;
+  } else return b;
+};
+
+console.log(less(8, 6));
+
+//2
+const evenOrOdd = (a) => {
+  if (a % 2 == 0) {
+    return "Число четное";
+  } else return "Число нечетное";
+};
+
+console.log(evenOrOdd(3));
+
+//3
+
