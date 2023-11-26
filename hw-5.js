be7faf3fb1490a9aaf434e8d19b29a4f3aba6796
p1ck23 <danil-vetrov47@mail.ru>
2023-11-26 05:02:05 +0400

    made third ex

---
+++ hw-5.js
@@ -0,0 +1,21 @@
+//1
+console.log("Ex1");
+let numbers = [1, 5, 4, 10, 0, 3];
+for (let index = 0; index < numbers.length; index++) {
+  if (numbers[index] == 10) break;
+  console.log(numbers[index]);
+}
+
+//2
+console.log("Ex2");
+let numbersSecond = [1, 5, 4, 10, 0, 3];
+console.log(numbersSecond.indexOf(4));
+
+//3
+console.log("Ex3");
+let numbersThird = [1, 3, 5, 10, 20];
+console.log(numbersThird.join(" "));
+
+//4
+console.log("Ex4");
+
