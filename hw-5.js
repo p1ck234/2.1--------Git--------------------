10a0b51402850d54e0d4bf15c53fcb4c3961a2e8
p1ck23 <danil-vetrov47@mail.ru>
2023-11-26 05:50:34 +0400

    Got the whole main part done

--- hw-5.js
+++ hw-5.js
@@ -50,3 +50,64 @@ console.log(arr2);
 
 //7
 console.log("Ex7");
+let arr3 = [9, 8, 7, 6, 5];
+let answerUser = Number(prompt("Угадай число"));
+for (let i = 0; i < arr3.length; i++) {
+  if (arr3[i] == answerUser) {
+    console.log("Угадал");
+    break;
+  } else console.log("Не угадал");
+}
+
+//8
+console.log("Ex8");
+let string = "abcdef";
+let chartArr = string.split("");
+console.log(chartArr.reverse().join(""));
+
+//9
+console.log("Ex9");
+let arr9 = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+console.log(arr9.join());
+
+//10
+console.log("Ex10");
+let arr10 = [5, 2, 8, 3, 6, 1, 7, 4, 9, 10];
+
+for (let i = 0; i < arr10.length - 1; i++) {
+  let currentElement = arr10[i];
+  let nextElement = arr10[i + 1];
+  let sum = currentElement + nextElement;
+  console.log(`Сумма элементов: ${currentElement} и ${nextElement} = ${sum}`);
+}
+
+//11
+console.log("Ex11");
+const squaresArr = (string) => {
+  let arr11 = stringArr.split(" ");
+  console.log(arr11.map((el) => el ** 2));
+};
+//let stringArr = prompt("Введите массив целых чисел через пробелы");
+//squaresArr(stringArr);
+
+//12
+console.log("Ex12");
+const getLengthWords = (arr) => {
+  console.log(arr.map((el) => el.length));
+};
+let arr12 = ["слово", "", "слог", "длинное предложение", "буква"];
+getLengthWords(arr12);
+
+//13
+console.log("Ex13");
+function filterPositive(array) {
+  console.log(array.filter((el) => el < 0));
+}
+
+filterPositive([-1, 0, 5, -10, 56]); // => [-1, -10]
+filterPositive([-25, 25, 0, -1000, -2]); // => [-25, -1000, -2]
+
+
