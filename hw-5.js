c0d56ff8575f51e3906505f51fc36c99ff3f6c8c
p1ck23 <danil-vetrov47@mail.ru>
2023-11-26 05:19:23 +0400

    make seven ex

--- hw-5.js
+++ hw-5.js
@@ -18,4 +18,35 @@ console.log(numbersThird.join(" "));
 
 //4
 console.log("Ex4");
+let doubleArr = [[], [], []];
+for (let i = 0; i < doubleArr.length; i++) {
+  for (let j = 0; j < doubleArr.length; j++) {
+    doubleArr[i][j] = 1;
+  }
+}
 
+console.log(doubleArr);
+// for (const arrIn of doubleArr) {
+//   for (const element of arrIn) {
+//     console.log(element);
+//   }
+// }
+
+//5
+console.log("Ex5");
+let arr = [1, 1, 1];
+arr.push(2, 2, 2);
+console.log(arr);
+// arr.forEach((el) => {
+//   console.log(arr[el]);
+// });
+
+//6
+console.log("Ex6");
+let arr2 = [9, 8, 7, "a", 6, 5];
+arr2.sort();
+arr2.pop();
+console.log(arr2);
+
+//7
+console.log("Ex7");
