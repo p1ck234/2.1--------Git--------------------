8e6b55c960092fb32f993d21a8df83203320d1a3
p1ck23 <danil-vetrov47@mail.ru>
2023-12-05 01:15:05 +0400

    Started doing mission six

--- hw-6.js
+++ hw-6.js
@@ -1,3 +1,40 @@
 //1
-let words = prompt("Введите строку");
-console.log(words.toUpperCase());
+console.log("#1");
+//let words = prompt("Введите строку");
+//console.log(words.toUpperCase());
+
+//2
+console.log("#2");
+const searchStart = (arrStr = [], search) => {
+  arrStr.forEach((word) => {
+    if (word.toLowerCase().includes(search.toLowerCase())) {
+      console.log(word);
+    }
+  });
+};
+searchStart(["Кошка", "Кит", "Комар", "Носорог"], "ко"); // ['кошка', 'комар']
+searchStart(["яблоко", "груша", "гриб", "огурец"], "гру"); // ['груша']
+searchStart(["Дом", "Банк", "Больница", "Театр"], "Кино"); // []
+
+//3
+console.log("#3");
+let a = 32.58884;
+console.log(`До меньшего целого: ${Math.floor(a)}
+До большего целого: ${Math.ceil(a)}
+До ближайшего целого: ${Math.round(a)}`);
+
+//4
+console.log("#4");
+let arr = [52, 53, 49, 77, 21, 32];
+console.log(`Наименьшее - ${Math.min(...arr)}
+Наибольшее - ${Math.max(...arr)}`);
+
+//5
+console.log("#5");
+const getRandom = () => {
+  console.log(Math.floor(Math.random() * 10) + 1);
+};
+getRandom();
+
+//6 
+console.log('#6');
