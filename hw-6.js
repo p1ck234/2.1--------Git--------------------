353920a1ca0fb1f483df018d38b0f926da40fe62
p1ck23 <danil-vetrov47@mail.ru>
2023-12-05 01:30:35 +0400

    did all the assignments

--- hw-6.js
+++ hw-6.js
@@ -36,5 +36,78 @@ const getRandom = () => {
 };
 getRandom();
 
-//6 
-console.log('#6');
+//6
+console.log("#6");
+const generateRandomArray = (maxNumber) => {
+  let arrayLenght = maxNumber / 2;
+  let arr = [];
+  for (let i = 0; i < arrayLenght; i++) {
+    arr.push(Math.floor(Math.random() * maxNumber + 1));
+  }
+  return arr;
+};
+console.log(generateRandomArray(10));
+
+//7
+console.log("#7");
+const getRandomInteger = (min, max) => {
+  return Math.round(Math.random() * (max - min)) + min;
+};
+console.log(getRandomInteger(1, 5));
+
+//8
+console.log("#8");
+let today = new Date();
+console.log(today);
+
+//9
+console.log("#9");
+let currentDate = new Date();
+currentDate.setDate(currentDate.getDate() + 73);
+console.log(currentDate);
+
+//10
+console.log("#10");
+const formatDateTime = (date) => {
+  let months = [
+    "января",
+    "февраля",
+    "марта",
+    "апреля",
+    "мая",
+    "июня",
+    "июля",
+    "августа",
+    "сентября",
+    "октября",
+    "ноября",
+    "декабря",
+  ];
+
+  let daysOfWeek = [
+    "воскресенье",
+    "понедельник",
+    "вторник",
+    "среда",
+    "четверг",
+    "пятница",
+    "суббота",
+  ];
+
+  let day = date.getDate();
+  let month = months[date.getMonth()];
+  let year = date.getFullYear();
+  let dayOfWeek = daysOfWeek[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  let formattedDate = `Дата: ${day} ${month} ${year} - это ${dayOfWeek}.`;
+  let formattedTime = `Время: ${hours}:${minutes}:${seconds}`;
+  return `${formattedDate}\n${formattedTime}`;
+};
+let time = new Date();
+let formattedDateTime = formatDateTime(time);
+console.log(formattedDateTime);
+
+
